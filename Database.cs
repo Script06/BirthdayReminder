c97dcc9b4e89a5edc212c52476aefe3a378c44fc
Script06 <61908564+Script06@users.noreply.github.com>
2024-07-31 17:17:48 +0300

    first commit
    first commit

---
+++ Database.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using BirthdayReminder;
+using Dapper;
+using Microsoft.Data.Sqlite;
+
+public class Database
+{
+    private string _connectionString;
+
+    public Database(string connectionString)
+    {
+        _connectionString = connectionString;
+        InitializeDatabase();
+    }
+
+    private void InitializeDatabase()
+    {
+        using (var connection = new SqliteConnection(_connectionString))
+        {
+            connection.Execute(@"
+                CREATE TABLE IF NOT EXISTS People (
+                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    LastName TEXT NOT NULL,
+                    FirstName TEXT NOT NULL,
+                    BirthDate TEXT NOT NULL
+                )
+            ");
+        }
+    }
+
+    public IEnumerable<Person> GetAllPeople()
+    {
+        using (var connection = new SqliteConnection(_connectionString))
+        {
+            return connection.Query<Person>("SELECT * FROM People");
+        }
+    }
+
+    public IEnumerable<Person> GetUpcomingBirthdays()
+    {
+        using (var connection = new SqliteConnection(_connectionString))
+        {
+            var today = DateTime.Today;
+            var nextWeek = today.AddDays(7);
+            return connection.Query<Person>(
+                "SELECT * FROM People WHERE strftime('%m-%d', BirthDate) BETWEEN strftime('%m-%d', @Today) AND strftime('%m-%d', @NextWeek)",
+                new { Today = today, NextWeek = nextWeek });
+        }
+    }
+
+    public void AddPerson(Person person)
+    {
+        using (var connection = new SqliteConnection(_connectionString))
+        {
+            connection.Execute("INSERT INTO People (LastName, FirstName, BirthDate) VALUES (@LastName, @FirstName, @BirthDate)", person);
+        }
+    }
+
+    public void UpdatePerson(Person person)
+    {
+        using (var connection = new SqliteConnection(_connectionString))
+        {
+            connection.Execute("UPDATE People SET LastName = @LastName, FirstName = @FirstName, BirthDate = @BirthDate WHERE Id = @Id", person);
+        }
+    }
+
+    public void DeletePerson(int id)
+    {
+        using (var connection = new SqliteConnection(_connectionString))
+        {
+            connection.Execute("DELETE FROM People WHERE Id = @Id", new { Id = id });
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,110 @@
+﻿using BirthdayReminder;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var database = new Database("Data Source=people.db");
+
+        while (true)
+        {
+            Console.WriteLine("1. Отобразить весь список ДР");
+            Console.WriteLine("2. Отобразить сегодняшние и ближайшие ДР");
+            Console.WriteLine("3. Добавить запись");
+            Console.WriteLine("4. Удалить запись");
+            Console.WriteLine("5. Редактировать запись");
+            Console.WriteLine("0. Выход");
+            Console.Write("Выберите опцию: ");
+            var choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    ShowAllBirthdays(database);
+                    break;
+                case "2":
+                    ShowUpcomingBirthdays(database);
+                    break;
+                case "3":
+                    AddPerson(database);
+                    break;
+                case "4":
+                    DeletePerson(database);
+                    break;
+                case "5":
+                    EditPerson(database);
+                    break;
+                case "0":
+                    return;
+                default:
+                    Console.WriteLine("Неверная опция. Попробуйте снова.");
+                    break;
+            }
+        }
+    }
+
+    static void ShowAllBirthdays(Database database)
+    {
+        Console.Clear();
+        var people = database.GetAllPeople();
+        foreach (var person in people)
+        {
+            Console.WriteLine($"{person.Id}. {person.LastName} {person.FirstName} - {person.BirthDate.ToShortDateString()}");
+            Console.WriteLine("---------------------------------------------------");
+        }
+    }
+
+    static void ShowUpcomingBirthdays(Database database)
+    {
+        Console.Clear();
+        var people = database.GetUpcomingBirthdays();
+        foreach (var person in people)
+        {
+            Console.WriteLine($"{person.Id}. {person.LastName} {person.FirstName} - {person.BirthDate.ToShortDateString()}");
+            Console.WriteLine("---------------------------------------------------");
+        }
+        
+    }
+
+    static void AddPerson(Database database)
+    {
+        Console.Clear();
+        Console.Write("Введите фамилию: ");
+        var lastName = Console.ReadLine();
+        Console.Write("Введите имя: ");
+        var firstName = Console.ReadLine();
+        Console.Write("Введите дату рождения (гггг-мм-дд): ");
+        var birthDate = DateTime.Parse(Console.ReadLine());
+
+        var person = new Person { LastName = lastName, FirstName = firstName, BirthDate = birthDate };
+        database.AddPerson(person);
+    }
+
+    static void DeletePerson(Database database)
+    {
+        Console.Clear();
+        ShowAllBirthdays(database);
+        Console.Write("Введите ID записи для удаления: ");
+        var id = int.Parse(Console.ReadLine());
+        database.DeletePerson(id);
+    }
+
+    static void EditPerson(Database database)
+    {
+        Console.Clear();
+        ShowAllBirthdays(database);
+        Console.Write("Введите ID записи для редактирования: ");
+        var id = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите новую фамилию: ");
+        var lastName = Console.ReadLine();
+        Console.Write("Введите новое имя: ");
+        var firstName = Console.ReadLine();
+        Console.Write("Введите новую дату рождения (гггг-мм-дд): ");
+        var birthDate = DateTime.Parse(Console.ReadLine());
+
+        var person = new Person { Id = id, LastName = lastName, FirstName = firstName, BirthDate = birthDate };
+        database.UpdatePerson(person);
+    }
+}
